96be13603debd79b1323a149bb51a2baeefc02cb
padolekeshav095 <padolekeshav095@gmail.com>
2025-02-27 12:03:40 +0530

    added LoginPage1 file

---
+++ src/Assignments/LoginPage_Demo.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class LoginPage_Demo {
+
+	public static void main(String[] args) {
+	
+		int i = 10;
+		System.out.println(i);
+
+	}
+
+}
---
+++ src/Assignments/Shopping.java
@@ -0,0 +1,24 @@
+package Assignments;
+                            //Method Overloading
+public class Shopping {
+
+
+	public void login()
+	{
+		
+	}
+	public void login (String username, String password)
+	{
+		
+	}
+	public void login (String username, String password, int otp)
+	{
+		
+	}
+	public static void main(String[] args) {
+		
+		Shopping shop = new Shopping();
+		shop.login();
+	}
+
+}
